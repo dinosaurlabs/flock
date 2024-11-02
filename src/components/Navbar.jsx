6f6984284aa2eb159f40693fb4fcd84221fda6c2
Kenny Morales <kenny-morales@outlook.com>
2024-11-02 16:25:13 -0500

    Set up structure for the students

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Navbar() {
+  // Manu and Toni
+  return <nav></nav>;
+}
+
+export default Navbar;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* Hero section (Wil and Gannett) */}
+      {/* How it works section (Riya and Gunnar)*/}
+      {/* Frequently asked section (Nina and Analise)*/}
     </div>
   );
 }
