97b6501629da38275d2734221276553ecba12121
Kenny Morales <kenny-morales@outlook.com>
2024-11-02 18:58:22 -0500

    Update Home component to add an empty className for future styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
+    <div className="">
       {/* Hero section (Wil and Gannett) */}
       {/* How it works section (Riya and Gunnar)*/}
       {/* Frequently asked section (Nina and Analise)*/}
