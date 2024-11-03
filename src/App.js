b969be7c99e34e49f125cb478c9d3cbafc844ccb
Kenny Morales <kenny-morales@outlook.com>
2024-11-02 19:24:44 -0500

    Refactor App and Home components to improve structure and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // App.js
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/Home.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-surface dark:bg-surface-dark text-black dark:text-white">
+      <div className="min-h-screen text-black bg-surface dark:bg-surface-dark dark:text-white">
+        <NavBar />
         <Routes>
           <Route path="*" element={<Home />} />
         </Routes>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen text-black bg-surface dark:bg-surface-dark dark:text-white">
       {/* Hero section (Wil and Gannett) */}
       {/* How it works section (Riya and Gunnar)*/}
       {/* Frequently asked section (Nina and Analise)*/}
