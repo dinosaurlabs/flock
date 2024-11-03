212fc370956de0c3077ac872148ae50dd5e18bb5
Kenny Morales <kenny-morales@outlook.com>
2024-11-02 20:44:33 -0500

    Finished Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import LogoIcon from "./SVGs/logos/FlockIcon";
+import LogoFull from "./SVGs/logos/FlockLogo";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   // Manu and Toni
-  return <nav></nav>;
+  return (
+    <nav className="fixed z-10 flex border-b-border dark:border-b-border-dark border-b-[1px] items-center justify-between w-full px-6 py-4">
+      <LogoFull />
+
+      {/* TODO: Route this in the future */}
+      <Link className="font-bold text-secondary dark:text-secondary-dark font-display">
+        CREATE A FLOCK
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
