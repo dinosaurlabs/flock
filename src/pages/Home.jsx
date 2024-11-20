44526fe5448b300497374db88cba799c26e1fa09
wlehan <williamclehan@gmail.com>
2024-11-20 09:32:52 -0600

    Added project elements to home screen.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,46 @@
 import React from "react";
+import Typography from "../components/TextStyles/Typography";
+import heroImage from "../assets/images/home-hero.png"
+import Pablo from "../components/SVGs/Pablo/Pablo.jsx";
 
 function Home() {
   return (
     <div className="min-h-screen text-black bg-surface dark:bg-surface-dark dark:text-white">
-      {/* Hero section (Wil and Gannett) */}
+      {/* Hero section (Wil and Gannett) */
+        <>
+            <div style={{width: 200}} className="flex flex-col gap-4" property="wrap">
+              
+              <Typography>
+                  <Typography>
+                    Effortlessly
+                  </Typography>
+                  <Typography>
+                    Organize and Meet Up in Seconds
+                  </Typography>
+              </Typography>
+              <Typography>
+                Flock lets you organize and join meetups in just a few clicks. Imagine When2Meet with a better design and user experience.
+              </Typography>
 
-      {/* How it works section (Riya and Gunnar)*/}
+              <div color="primary" className="flex">
+                <button>
+                  Join a Flock
+                </button>
+                <button>
+                  Donate
+                </button>
+              </div>
 
-      {/* Frequently asked section (Nina and Analise)*/}
+              <Pablo style={{ }}>
+
+              </Pablo>
+              <img src={heroImage} alt="Hero image." style={{borderRadius: '15px'}}>
+
+              </img>
+          </div>
+        </>
+      }
+
     </div>
   );
 }
