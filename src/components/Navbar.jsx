1947a50bbd7fa62284502fca715999d7a816e0b9
Caleb Kelly <ckelly25@huskers.unl.edu>
2024-11-20 21:14:07 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Typography from "../components/TextStyles/Typography";
+import FlockLogo from "../components/SVGs/logos/FlockLogo"
 
 function Navbar() {
-  // Manu
-  return <nav></nav>;
+  
+    return (<nav className="px-4 pb-3 pt-5" style={{borderBottom: "1px inset solid grey"} }>
+        
+        <div className="flex pt-3">
+            <FlockLogo />
+            <div style={{flex:1}}></div>
+            <Typography textStyle="" color="secondary">CREATE A FLOCK</Typography>
+        </div>
+    </nav>);
 }
 
 export default Navbar;
