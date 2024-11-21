630e1c01dbf538db3a33e627d34fb80f683b5cc4
Caleb Kelly <ckelly25@huskers.unl.edu>
2024-11-20 21:21:50 -0600

    Fixed Navbar.js to include fixed attribute and a set width.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import FlockLogo from "../components/SVGs/logos/FlockLogo"
 
 function Navbar() {
   
-    return (<nav className="px-4 pb-3 pt-5" style={{borderBottom: "1px inset solid grey"} }>
+    return (<nav className="px-4 pb-3 pt-5 fixed" style={{borderBottom: "1px inset solid grey", width: "393px"} }>
         
         <div className="flex pt-3">
             <FlockLogo />
