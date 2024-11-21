8bf2640e6db8e5cafd54633e66f6e586a2c33ed1
Kenny Morales <kenny-morales@outlook.com>
2024-11-20 21:15:34 -0600

    Refactor Button component for improved styling and structure

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Typography from "./TextStyles/Typography";
 
-function Button({phrase}) {
-    return (
-        <div>
-      <button
-        className="px-8 rounded-md py-6 bg-primary"
-         pr-16px
-        >
-          <Typography textStyle= "label-lg">{phrase}</Typography>
-        </button>
-        </div>
-      );
-      
-  }
+function Button({ phrase }) {
+  return (
+    <div>
+      <button className="px-4 py-3 rounded-md bg-primary" pr-16px>
+        <Typography textStyle="label-lg">{phrase}</Typography>
+      </button>
+    </div>
+  );
+}
 
-export default Button
+export default Button;
